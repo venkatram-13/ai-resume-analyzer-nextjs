1afd40eba0cc6a2f666707d8855ad08888796405
venkatram-13 <22501a4464@pvpsit.ac.in>
2025-09-25 05:37:34 +0530

    Fix ResumeCard Import Path

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
-import ResumeCard from "~/components/ResumeCard";
+import ResumeCard from "../components/ResumeCard";
 import {usePuterStore} from "~/lib/puter";
 import {Link, useNavigate} from "react-router";
 import {useEffect, useState} from "react";
